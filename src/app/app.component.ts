ec094ad2cabe5cac054e6d1dc6b5fd244e41bcb9
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-02-19 21:02:04 -0300

    [fix] bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, UserComponent],
+  imports: [CommonModule, RouterModule, UserComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styles: ['']  // Puedes añadir estilos aquí si es necesario
 })
